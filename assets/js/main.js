346b365ed9544df2dc288e1cbe6fd629a4e5100a
Cristian Nistor <Cristian.Nistor@everymatrix.com>
2018-07-26 17:21:47 +0300

    Improved home.

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+/**
+ * Main JS file for poveglia
+ */
+
+jQuery(document).ready(function($) {
+
+    var config = {
+        'share-selected-text': true,
+        'load-more': true,
+        'infinite-scroll': false,
+        'infinite-scroll-step': 1,
+        'disqus-shortname': 'hauntedthemes-demo'
+    };
+
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+        h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
+        readLaterPosts = [];
+
+   
+
+});
