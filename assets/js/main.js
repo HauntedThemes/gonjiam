a41fa55581481488f6db1f746c25edf27aa70798
Cristian Nistor <contact@nistorcristian.com>
2018-08-01 00:04:57 +0300

    Improved Search and Bookmarks.

--- assets/js/main.js
+++ assets/js/main.js
@@ -145,6 +145,8 @@ jQuery(document).ready(function($) {
           var url = [location.protocol, '//', location.host].join('');
 
           $('header .counter').removeClass('hidden').text(readLaterPosts.length);
+          $('.bookmark-container').removeClass('no-bookmarks');
+
           var filter = readLaterPosts.toString();
           filter = "id:["+filter+"]";
 
@@ -152,8 +154,9 @@ jQuery(document).ready(function($) {
 
               $('.bookmark-container').empty();
 
+
               $.each(data.posts, function(index, val) {
-                console.log(val);
+              console.log(val);
                 var tag;
                 if (val.tags.length) {
                   tag = '<span class="tags"><a href="/tag/'+ val.tags[0].slug +'">'+ val.tags[0].name +'</a></span>';
@@ -167,11 +170,11 @@ jQuery(document).ready(function($) {
                         </p> \
                     </div> \
                     <div class="post-meta"> \
-                        <time datetime="'+ val.pubDate +'">'+ val.pubDate +'</time>'+ tag +' \
+                        <time datetime="'+ prettyDate(val.created_at) +'">'+ prettyDate(val.created_at) +'</time>'+ tag +' \
                         <div class="inner"> \
                           <a href="'+ val.link +'#disqus_thread" class="count-comments"></a> \
                           <a href="https://twitter.com/share?text='+ encodeURIComponent(val.title) +'&amp;url='+ url + val.link +'" class="twitter" onclick="window.open(this.href, \'share-twitter\', \'width=550,height=235\');return false;" data-toggle="tooltip" data-placement="top" title="Share on Twitter"><i class="fab fa-twitter"></i></a> \
-                          <a href="#" class="read-later" data-id="'+ val.id +'"><i class="far fa-bookmark"></i></a> \
+                          <a href="#" class="read-later active" data-id="'+ val.id +'"><i class="far fa-bookmark"></i></a> \
                         </div> \
                     </div> \
                   </article> \
@@ -185,7 +188,6 @@ jQuery(document).ready(function($) {
                       var id = $(this).attr('data-id');
                       if ($(this).hasClass('active')) {
                           removeValue(readLaterPosts, id);
-                          $('.tooltip').remove();
                       }else{
                           readLaterPosts.push(id);
                       };
@@ -199,31 +201,36 @@ jQuery(document).ready(function($) {
 
           });
       }else{
+          $('.bookmark-container').addClass('no-bookmarks');
           $('header .counter').addClass('hidden');
-          $('.bookmark-container').append('<p class="no-bookmarks">You haven\'t yet saved any bookmarks. To bookmark a post, just click <i class="far fa-bookmark"></i>.</p>')
+          $('.bookmark-container').append('<p>You haven\'t yet saved any bookmarks. To bookmark a post, just click <i class="far fa-bookmark"></i>.</p>')
       };
-
   }
 
   function prettyDate(date) {
-      var d = new Date(date);
-      var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-          return d.getDate() + ' ' + monthNames[d.getMonth()] + ' ' + d.getFullYear();
+    var d = new Date(date);
+    var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        return d.getDate() + ' ' + monthNames[d.getMonth()] + ' ' + d.getFullYear();
   };
 
   function removeValue(arr) {
-      var what, a = arguments, L = a.length, ax;
-      while (L > 1 && arr.length) {
-          what = a[--L];
-          while ((ax= arr.indexOf(what)) !== -1) {
-              arr.splice(ax, 1);
-          }
-      }
-      return arr;
+    var what, a = arguments, L = a.length, ax;
+    while (L > 1 && arr.length) {
+        what = a[--L];
+        while ((ax= arr.indexOf(what)) !== -1) {
+            arr.splice(ax, 1);
+        }
+    }
+    return arr;
   }
 
   $('#search-field').on('click', function(event) {
     $('#drawer').addClass('search-focus');
+  });
+
+  $('.close-search').on('click', function(event) {
+    event.preventDefault();
+    $('#drawer').removeClass('search-focus');
   });
 
   $('.modal').on('mouseup', function(e) {
